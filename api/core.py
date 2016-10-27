c9b240502a27bcf22642a0c7f281091aab7193a9
Mike <senselessvision@gmail.com>
2016-10-27 20:06:21 +0200

    message markdown fix

--- api/core.py
+++ api/core.py
@@ -63,11 +63,11 @@ def parse_event(json_data, post_content=''):
             assignee = 'empty'
 
         if webevent.endswith('created'):
-            post_content = ''.join(['##### ', display_name, ' has created issue: [ ', issue_id, ' ] (', issue_url,
+            post_content = ''.join(['##### ', display_name, ' has created issue: [', issue_id, '](', issue_url,
                                     ')\n\n', summary, '\n\n> ', description, '\n\n###### Priority: ', priority,
                                     ' | Assignee: ', assignee, '\r\n'])
         elif any([webevent.endswith('updated'), webevent.endswith('deleted')]):
-            post_content = ''.join(['##### ', display_name, ' has ', webevent[-7:], ' issue: [ ', issue_id, ' ] (',
+            post_content = ''.join(['##### ', display_name, ' has ', webevent[-7:], ' issue: [', issue_id, '](',
                                     issue_url, ')\n\n', summary, '\n\n###### Priority: ', priority, ' | Assignee: ',
                                     assignee, '\r\n'])
 
