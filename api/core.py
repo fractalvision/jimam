d1eb0387917490dddde690ab31fbc1e131dfa545
Mike <senselessvision@gmail.com>
2016-10-28 21:22:22 +0200

    better and better

--- api/core.py
+++ api/core.py
@@ -110,4 +110,4 @@ def parse_event(json_data):
             else:
                 log('Skipped unhandled event.')
 
-        return post_content.getvalue().encode('utf8')
+        return str(post_content.getvalue())
